5f10b171cf835addde90e9290b3e422e0d3d635c
FERNANDO MENCIA <131507842+FERNANDO-MENCIA-51@users.noreply.github.com>
2023-10-06 11:00:16 -0500

    Add files via upload

---
+++ 24_LuisMencia_GIT/S10_24.Java/src/s10_24/java/medico_24.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s10_24.java;
+
+/**
+ *
+ * @author FERNANDO
+ */
+public class medico_24 {
+    
+    //atributos
+    private String especialidad;
+    
+    //metodo
+    
+    public void atender() {
+    }
+    
+    
+}
---
+++ 24_LuisMencia_GIT/S10_24.Java/src/s10_24/java/seleccionFutbol_24.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s10_24.java;
+
+/**
+ *
+ * @author FERNANDO
+ */
+public class seleccionFutbol_24 {
+    //clase padre
+    //atributos
+    
+   protected int id;
+    protected String nombre;
+    protected String apellidos;
+    protected int edad;
+    
+    //metodos
+    public void concentrarse() {
+    }
+    
+    public void viajar (){
+    }
+   
+}
